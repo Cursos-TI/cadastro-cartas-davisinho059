2fa0c89d46f7df34f0aba4551b449585b5dc6af0
davisinho059 <davisinho059@gmail.com>
2025-12-29 12:24:40 +0000

    colocando variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,26 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  printf("criando jogo carta super trunfo")
 
+  // Área para definição das variáveis para armazenar as propriedades das cidades
+//carta 01
+char estado=piaui
+char codigo cada carta=A01
+char  cidade=parnaiba
+int populaçao=162.159
+float area km2=435,57
+float pib=2,6 b
+int pontos=15
+
+//carta 02
+char estado=acre
+char codigo da carta=B02
+char cidade=rio branco
+int populaçao=387,85
+float area km2=8.836
+float=26 
+int pontos=15
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
